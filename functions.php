d101b30b53d52b324b1c21e4353f7c83b805ba8d
eugen_lisov <eugen.lisov@gmail.com>
2022-05-10 17:37:22 +0300

    Implemented an ACF fix

--- functions.php
+++ functions.php
@@ -29,3 +29,25 @@ include( get_template_directory() . '/inc/scripts.php' );
 
 // Gutenberg blocks
 include( get_template_directory() . '/inc/acf_gutenberg.php' );
+
+
+// Implemented a fix for ACF Image Blocks caused by WP5.9.1. Solution is taken from this Support thread:
+// https://support.advancedcustomfields.com/forums/topic/update-to-wp-5-9-1-breaks-media-selection/#post-152581
+// It currently works, but probably we should get an updated version of ACF.
+
+function acf_filter_rest_api_preload_paths( $preload_paths ) {
+	global $post;
+	$rest_path    = rest_get_route_for_post( $post );
+	$remove_paths = array(
+		add_query_arg( 'context', 'edit', $rest_path ),
+		sprintf( '%s/autosaves?context=edit', $rest_path ),
+	);
+
+	return array_filter(
+		$preload_paths,
+		function( $url ) use ( $remove_paths ) {
+			return ! in_array( $url, $remove_paths, true );
+		}
+	);
+}
+add_filter( 'block_editor_rest_api_preload_paths', 'acf_filter_rest_api_preload_paths', 10, 1 );
